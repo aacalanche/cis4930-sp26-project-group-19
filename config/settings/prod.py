a96686374d20a09cfcc10c591d2ffbfc16121df4
Ares <agregory3@fsu.edu>
2026-04-23 23:50:35 -0400

    fixes security issues

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,9 +1,15 @@
 from .base import *
+from decouple import config
 
 DEBUG = False
 
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    cast=lambda v: [s.strip() for s in v.split(',')],
-    default=''
-)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+
+SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", cast=bool, default=True)
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
